06195e9f3dfbdc4ea46146a563059912f2b54970
Sanjaydev-del <official.sanjayx@gmail.com>
2026-01-29 08:04:22 +0530

    Update navbar brand to Sanjay.Devfolio

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleTheme }) => {
     }`}>
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
         <a href="#" className="text-2xl font-extrabold tracking-tighter text-indigo-600 dark:text-indigo-400">
-          SANJAY<span className="text-slate-900 dark:text-white">.</span>
+          Sanjay<span className="text-slate-900 dark:text-white">.Devfolio</span>
         </a>
 
         {/* Desktop Links */}
